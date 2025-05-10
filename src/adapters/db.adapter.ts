03a61185fc7c7d41670fe8492cfafb5a874a710c
Slaega <smatsoula19@gmail.com>
2025-05-10 22:26:51 +0100

    refactor(db-adapter): change findFirst to findOne

--- src/adapters/db.adapter.ts
+++ src/adapters/db.adapter.ts
@@ -1,12 +1,13 @@
+import { EntityName } from "@mikro-orm/core";
+import { Model } from "sequelize-typescript";
+import { EntityTarget } from "typeorm";
+type ModelLiteralValue =
+  | string
+  | (new () => Model<any, any>)
+  | EntityName<any>
+  | EntityTarget<any>;
 export interface DBAdapter {
-    findFirst(
-        model: string,
-        where: any
-    ): Promise<any>;
+  findOne(model: ModelLiteralValue, where: any): Promise<any>;
 
-
-    count(
-        model: string,
-        where: any
-    ): Promise<any>;
+  count(model: ModelLiteralValue, where: any): Promise<any>;
 }
--- src/adapters/prisma.adapter.ts
+++ src/adapters/prisma.adapter.ts
@@ -5,10 +5,17 @@ import { DBAdapter } from "./db.adapter";
 export class PrismaAdapter implements DBAdapter {
     constructor(private readonly prisma: PrismaClient) {}
 
-    async findFirst(
+    async findOne(
         model: string,
         where: any
     ): Promise<any> {
         return await this.prisma[model].findFirst({ where });
+    }
+
+    async count(
+        model: string,
+        where: any
+    ): Promise<any> {
+        return await this.prisma[model].count({ where });
     }
 }
