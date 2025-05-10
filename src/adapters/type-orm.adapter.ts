b1676467a3bd7e8340eddddd60b8273435f242c6
Slaega <smatsoula19@gmail.com>
2025-05-10 22:28:07 +0100

    refactor(typeorm): add typeorm adapter

---
+++ src/adapters/type-orm.adapter.ts
@@ -0,0 +1,18 @@
+import { DataSource, EntityTarget } from 'typeorm';
+import { DBAdapter } from './db.adapter';
+
+export class TypeOrmAdapter implements DBAdapter {
+    constructor(private readonly dataSource: DataSource) {}
+    async count(model: EntityTarget<any>, where: any): Promise<any> {
+        const repository = this.dataSource.getRepository(model);
+        return await repository.countBy(where);
+    }
+
+    async findOne(
+        model: EntityTarget<any>,
+        where: any
+    ): Promise<any> {
+        const repository = this.dataSource.getRepository(model);
+        return await repository.findOne(where);
+    }
+}
